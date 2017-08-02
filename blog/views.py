768cc292fbfd38c89a2e584d0aa8a5829e8e22ca
Your Name <you@example.com>
2017-08-02 16:58:13 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+from django.utils import timezone
+
+from .models import Post
+
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #  post = 쿼리셋의 이름 
+
+
+	return render(request, 'blog/post_list.html', {'posts': posts})	
+
